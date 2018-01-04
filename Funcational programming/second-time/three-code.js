ef25017385b1fe0e4d2dd33e744303044f779c0c
黄巍 <huan1043269994@gmail.com>
2018-01-04 17:20:00 +0800

    [ADD] third chapter take a part/

--- Funcational programming/second-time/three-code.js
+++ Funcational programming/second-time/three-code.js
@@ -10,3 +10,59 @@ function reverseArgs(fn) {
     return fn(...args.reverse());
   };
 }
+
+function partialRight(fn, ...presetArgs) {
+  return reverseArgs(
+    partial(reverseArgs(fn), ...presetArgs.reverse())
+  );
+}
+
+function curry(fn, arity = fn.length) {
+  return (function nextCurried(prevArgs) {
+    return function curried(nextArgs) {
+      var args = prevArgs.concat([nextArgs]);
+
+      if (args.length >= arity) {
+        return fn(...args); 
+      } else {
+        return nextCurried(args);
+      }
+    };
+  })([]);
+}
+
+function looseCurry(fn, arity = fn.length) {
+  return (function nextCurried(prevArgs) {
+    return function curried(...nextArgs) {
+      var args = prevArgs.concat(nextArgs);
+
+      if (args.length >= arity) {
+        return fn(...args);
+      } else {
+        return nextCurried(args);
+      }
+    };
+  })([]);
+}
+
+function uncurry(fn) {
+  return function uncurried(...args) {
+    var ret = fn;
+
+    for (const i = 0; i < args.length; i++) {
+      ret = ret(args[i]);
+    }
+
+    return ret;
+  };
+}
+
+function unary(fn) {
+  return function onlyOneArg(arg) {
+    return fn(arg);
+  };
+}
+
+function identity(v) {
+  return v;
+}
